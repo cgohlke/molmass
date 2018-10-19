41a9cdb3ed538a1eb9bf8fb3ec34346f75f6f274
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 13:14:40 -0700

    Update molmass/elements_gui.py

--- molmass/elements_gui.py
+++ molmass/elements_gui.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 # elements_gui.py
 
@@ -8,19 +7,21 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of the copyright holders nor the names of any
-#   contributors may be used to endorse or promote products derived
-#   from this software without specific prior written permission.
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -33,13 +34,13 @@
 
 :Author: `Christoph Gohlke <https://www.lfd.uci.edu/~gohlke/>`_
 
-:Version: 2018.8.15
+:Version: 2018.10.18
 
 Requirements
 ------------
 * `CPython 2.7 <https://www.python.org>`_
 * `wxPython 3.0 <http://www.wxpython.org>`_
-* `Elements.py 2018.8.15 <https://www.lfd.uci.edu/~gohlke/>`_
+* `Elements.py 2018.10.18 <https://www.lfd.uci.edu/~gohlke/>`_
 
 Revisions
 ---------
@@ -73,7 +74,7 @@ class MainApp(wx.App):
     """Main application."""
 
     name = 'Periodic Table of Elements'
-    version = '2018.8.15'
+    version = '2018.10.18'
     website = 'https://www.lfd.uci.edu/~gohlke/'
     copyright = ('Christoph Gohlke\n'
                  'Laboratory for Fluorescence Dynamics\n'
