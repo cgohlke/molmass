982d586e6e09767489c914cc1d5a2b8e5d644aa7
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 23:16:20 -0700

    Update molmass/__init__.py

--- molmass/__init__.py
+++ molmass/__init__.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 # molmass/__init__.py
 
 from .elements import *
 
+from .molmass import __doc__, __all__, __version__
 from .molmass import *
-from .molmass import __doc__
-from .molmass import __version__
 
 from . import molmass_web as web
