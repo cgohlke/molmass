aabf724ee8ace15d1582023f7e61c4a32a08bfce
Christoph Gohlke <cgohlke@uci.edu>
2021-06-17 20:14:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,21 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
-readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
-                   code, re.MULTILINE | re.DOTALL).groups()[0]
+readme = re.search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)] +
-                   readme.splitlines()[1:])
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
 
-license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
-                    re.MULTILINE | re.DOTALL).groups()[0]
+license = re.search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
 
 license = license.replace('# ', '').replace('#', '')
 
@@ -39,7 +46,7 @@ setup(
     long_description=readme,
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
-    url='https://www.lfd.uci.edu/~gohlke/',
+    url='https://www.lfd.uci.edu/~gohlke/molmass',
     project_urls={
         'Bug Tracker': 'https://github.com/cgohlke/molmass/issues',
         'Source Code': 'https://github.com/cgohlke/molmass',
@@ -50,15 +57,14 @@ setup(
     entry_points={
         'console_scripts': [
             'molmass = molmass.molmass:main',
-            'molmass_web = molmass.molmass_web:main'
+            'molmass_web = molmass.molmass_web:main',
         ],
-        'gui_scripts': [
-            'elements_gui = molmass.elements_gui:main',
-        ]},
+        'gui_scripts': ['elements_gui = molmass.elements_gui:main'],
+    },
     extras_require={'gui': ['wxPython>=4.0']},
     license='BSD',
     platforms=['any'],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
@@ -66,9 +72,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
