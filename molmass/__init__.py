e78a2b0effd92f168ae093c5d9723b7c854974ef
Christoph Gohlke <cgohlke@cgohlke.com>
2022-10-18 10:06:47 -0700

    Update molmass/__init__.py

--- molmass/__init__.py
+++ molmass/__init__.py
@@ -1,8 +1,10 @@
 # molmass/__init__.py
 
-from .elements import *
-
 from .molmass import __doc__, __all__, __version__
 from .molmass import *
+from .elements import *
 
-from . import molmass_web as web
+from . import elements
+
+__all__ = __all__ + elements.__all__
+del elements
