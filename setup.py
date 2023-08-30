b46c3d4310c9724b5c542c5f4d718247651dc3ad
Christoph Gohlke <cgohlke@cgohlke.com>
2023-08-29 20:54:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 """Molmass package Setuptools script."""
 
-import sys
 import re
+import sys
 
 from setuptools import setup
 
@@ -16,10 +16,10 @@ def search(pattern, code, flags=0):
     return match.groups()[0]
 
 
-with open('molmass/molmass.py') as fh:
+with open('molmass/molmass.py', encoding='utf-8') as fh:
     code = fh.read()
 
-version = search(r"__version__ = '(.*?)'", code)
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
 
 description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
@@ -42,10 +42,10 @@ license = search(
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
-    with open('README.rst', 'w') as fh:
+    with open('README.rst', 'w', encoding='utf-8') as fh:
         fh.write(readme)
 
 setup(
@@ -54,6 +54,7 @@ setup(
     license='BSD',
     description=description,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Christoph Gohlke',
     author_email='cgohlke@cgohlke.com',
     url='https://www.cgohlke.com',
@@ -63,7 +64,7 @@ setup(
         # 'Documentation': 'https://',
     },
     packages=['molmass'],
-    package_data={'molmass': ['icon.png']},
+    package_data={'molmass': ['py.typed', 'icon.png']},
     entry_points={
         'console_scripts': [
             'molmass = molmass.molmass:main',
@@ -76,7 +77,7 @@ setup(
         'gui': ['wxPython>=4.0'],
     },
     platforms=['any'],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
@@ -84,9 +85,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
