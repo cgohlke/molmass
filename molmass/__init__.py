9ca5ca1924c85a544082fdf930ce918e01d93322
Christoph Gohlke <cgohlke@cgohlke.com>
2025-04-13 19:30:59 -0700

    Update molmass/__init__.py

--- molmass/__init__.py
+++ molmass/__init__.py
@@ -1,10 +1,22 @@
 # molmass/__init__.py
 
-from .molmass import __doc__, __all__, __version__
-from .molmass import *
-from .elements import *
-
 from . import elements
+from .elements import *
+from .molmass import *
+from .molmass import __all__, __doc__, __version__
 
 __all__ = __all__ + elements.__all__
 del elements
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
