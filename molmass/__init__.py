cab25d39b62122d0ef1c482b13d773f3e805de33
Christoph Gohlke <cgohlke@cgohlke.com>
2025-11-06 13:42:46 -0800

    Update molmass/__init__.py

--- molmass/__init__.py
+++ molmass/__init__.py
@@ -8,15 +8,29 @@ from .molmass import __all__, __doc__, __version__
 __all__ = __all__ + elements.__all__
 del elements
 
+# constants are repeated for documentation
 
-def _set_module() -> None:
-    """Set __module__ attribute for all public objects."""
-    globs = globals()
-    module = globs['__name__']
-    for item in __all__:
-        obj = globs[item]
-        if hasattr(obj, '__module__'):
-            obj.__module__ = module
+__version__ = __version__
+"""Molmass version string."""
 
+ELEMENTS = ELEMENTS
+"""Collection of chemical elements with lookup by number, symbol, and name.
 
-_set_module()
+An ordered collection of Element instances with lookup by
+atomic number (int), chemical symbol (str), or element name (str).
+"""
+
+ELEMENTARY_CHARGE = ELEMENTARY_CHARGE
+"""Elementary charge in coulombs."""
+
+ELECTRON = ELECTRON
+"""Electron particle."""
+
+PROTON = PROTON
+"""Proton particle."""
+
+NEUTRON = NEUTRON
+"""Neutron particle."""
+
+POSITRON = POSITRON
+"""Positron or antielectron particle."""
